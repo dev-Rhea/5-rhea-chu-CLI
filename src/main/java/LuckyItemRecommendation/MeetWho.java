e90bea1ece9369323302fef4040dd00301ac1a97
rhea <dev.rhea4444@gmail.com>
2024-05-24 10:52:33 +0900

    0524 무한루프 대체, 반복 코드 정리

--- src/main/java/LuckyItemRecommendation/MeetWho.java
+++ src/main/java/LuckyItemRecommendation/MeetWho.java
@@ -5,37 +5,10 @@ import java.util.Scanner;
 public class MeetWho extends UserChoice{
     public void MeetWho(){
         Scanner sc = new Scanner(System.in);
-        String[] who;
-        int whoChoice;
-        String whoMsg;
-
-        while (true){
-            who = new String[] {"1. 가 족", "2. 친 구", "3. 애 인", "4. 지 인"
+        String[] who = {"1. 가 족", "2. 친 구", "3. 애 인", "4. 지 인"
                     , "5. 교수님", "6. 상 사", "7. 낯선이", "8. 동 료"};
-            whoMsg = "누 구 와 만 날 건 가 요?";
-
-            MainMsg(whoMsg);
-            for(String s : who){
-                System.out.println(".･ ｡ ⊹ + ஐ ஐ ஐ ஐ " + s + " ஐ ஐ ஐ ஐ + ⊹ ｡ ･.");
-            }
-            ChoiceMsg();
-
-            try{
-                whoChoice = sc.nextInt();
-                switch (whoChoice){
-                    case 1, 2, 3, 4, 5, 6, 7, 8, 9 -> {
-                        GoingWhere();
-                    }
-                    default -> {
-                        ErrorMsg();
-                        continue;
-                    }
-                }
-            }
-            catch(Exception e){
-                ErrorMsg();
-                whoChoice = sc.nextInt(); // 컈치안에서 무한 반복 탈출 위해서 재 초기화
-            }
-        }
+        String whoMsg = "누 구 와 만 날 건 가 요?";
+        UserInputHandler(who, whoMsg, 8);
+        GoingWhere();
     }
 }
--- src/main/java/LuckyItemRecommendation/WhatDoing.java
+++ src/main/java/LuckyItemRecommendation/WhatDoing.java
@@ -5,39 +5,10 @@ import java.util.Scanner;
 public class WhatDoing extends GoingHow {
     public void WhatDo(){
         Scanner sc = new Scanner(System.in);
-        String[] doing;
-        int doingChoice;
-        String doingMsg;
-
-        while (true){
-            doing = new String[]{"1. 공 부", "2. 수 다", "3.액티비티", "4. 산 책",
+        String[] doing = {"1. 공 부", "2. 수 다", "3.액티비티", "4. 산 책",
                     "5. 업 무", "6. 관 람", "7. 수 영", "8. 독 서", "9.멍때리기"};
-            doingMsg = "무 엇 을 하 실 건 가 요?";
-
-            MainMsg(doingMsg);
-            for(String s : doing){
-                System.out.println(".･ ｡ ⊹ + ஐ ஐ ஐ ஐ " + s + " ஐ ஐ ஐ ஐ + ⊹ ｡ ･.");
-            }
-            ChoiceMsg();
-
-            try{
-                doingChoice = sc.nextInt();
-                switch (doingChoice){
-                    case 1, 2, 3, 4, 5, 6, 7, 8, 9 -> {
-                        LuckyPrint();
-                    }
-
-                    default -> {
-                        ErrorMsg();
-                        continue;
-                    }
-                }
-            }
-            catch(Exception e){
-                ErrorMsg();
-                doingChoice = sc.nextInt(); // 컈치안에서 무한 반복 탈출 위해서 재 초기화
-            }
-        }
+        String doingMsg = "무 엇 을 하 실 건 가 요?";
+        UserInputHandler(doing, doingMsg, 9);
+        LuckyPrint();
     }
-
 }
